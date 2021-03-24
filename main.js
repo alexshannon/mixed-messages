eeb2e96276df846430ef967089f16d778b0f07be
alexshannon <alexshannon2@gmail.com>
2021-03-23 21:50:48 -0700

    Update main.js
    Added pretty much all of the functionality other than stats on race, saving locally

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ async function main() {
   if(seriousOrSilly === 'serious' || seriousOrSilly === 'silly'){
     if(characterRole === 'ranged' || characterRole === 'melee' || characterRole === 'caster'){
         console.log(`Thanks! Making ${characterName} now...`)
-        newCharacter = charGen(characterName, characterRole, seriousOrSilly)
-        generateStats()
+        newCharacter = charGen(characterName, characterRole, generateType(characterRole), seriousOrSilly, generateStats(characterRole), generateQuirk(seriousOrSilly, characterName, characterRole), generateRace())
+        console.log(newCharacter)
       } else{
           input.write('Oh no! You entered an invalid role. Exiting.')
       }
@@ -31,22 +31,20 @@ async function main() {
 
 main();
 
-const charGen = (name, role, mood) => {
+//outputs the info of the character that's created by the program #todo - save to a local file
+const charGen = (name, role, type, mood, stats, quirk, race) => {
     return{
     Character_Name: name,
-    Character_Roll: role,
+    Character_Role: role,
+    Character_Class: type,
     Character_Mood: mood,
-    Character_Stats: [],
-    Character_Quirk: '',
-    set stats(chrStat){
-        this.Character_Stats.push(chrStat)
-    },
-    set quirk(charQuirk){
-        this.Character_Quirk = charQuirk
-    }
-    }
+    Character_Stats: stats,
+    Character_Quirk: quirk,
+    Character_Race: race,
+    } 
 }
 
+//this isn't called by itself - this is used in stat declaration
 function statRoll(){
     //follows dnd rules - gens 4 random d6, drops the lowest value
     //vanilla sort works here due to no double digits
@@ -55,40 +53,96 @@ function statRoll(){
     {    
         roll.push((Math.floor(Math.random() * 6) + 1))
     }
+    //sorts low to high, drops lowest, and returns the stat role
     roll.sort()
     roll.shift()
     return roll[0] + roll[1] + roll[2]
 }
 
-function generateStats(){
+
+function generateType(role){
+    const casterArray = ['Wizard', 'Warlock', 'Sorcerer', 'Druid'];
+    const meleeArray = ['Barbarian', 'Cleric', 'Fighter', 'Monk', 'Paladin', 'Rogue'];
+    const rangedArray = ['Bard', 'Ranger'];
+    //Randomly selects the class of the character based on role
+    let classRole = 0;
+    if(role === 'caster'){
+       classRole = Math.floor(Math.random()*casterArray.length)
+       return casterArray[classRole]
+    }
+    else if(role === 'melee'){
+        classRole = Math.floor(Math.random()*meleeArray.length)
+        return meleeArray[classRole]
+     }
+     else if(role === 'ranged'){
+        classRole = Math.floor(Math.random()*rangedArray.length)
+        return rangedArray[classRole]
+     }
+
+}
+
+function generateStats(charRole){
     //declares the various arrays used. statArray/attributeArray only hold the data; finalArray is the output
     const statArray = [];
     const attributeArray = [];
-    let finalArray = [];
+    const finalArray = [];
+    //populates the array with the 6 stat rolls
     do{
     statArray.push(statRoll())
     } while(statArray.length < 6)
+    //sorts the rolls high to low
     statArray.sort((a, b) => b - a);
-    console.log(newCharacter.Character_Roll)
 
-    if(newCharacter.Character_Roll === 'melee'){
+    //determines ordering of the stats based on the role
+    if(charRole === 'melee'){
         attributeArray.push('Str', 'Con', 'Dex', 'Cha', 'Int', 'Wis')
-    } else if(newCharacter.Character_Roll === 'ranged'){
+    } else if(charRole === 'ranged'){
         attributeArray.push('Dex', 'Cha', 'Str', 'Con', 'Int', 'Wis')
-    } else if(newCharacter.Character_Roll === 'caster'){
+    } else if(charRole === 'caster'){
         attributeArray.push('Wis', 'Int', 'Cha', 'Dex', 'Str', 'Con')
     }
+    //sorts the array to a new element in finalArray
     for(let i = 0; i< attributeArray.length; i++){
-        finalArray[i] = attributeArray[i] + statArray[i]
+        finalArray.push(`${attributeArray[i]}: ${statArray[i]}`)
     }
-    console.log(finalArray)
-    newCharacter.stats = finalArray
+    return finalArray
 }
 
-function generateQuirk(){
-    if(newCharacter.Character_Mood === 'serious'){
-        //serious quirk arrays
-    } else if(newCharacter.Character_Mood === 'silly'){
-        //silly quirk arrays
+function generateQuirk(charMood, charName, charRole){
+    //scoped a bit higher, but this was a way to do this with the least number of variables
+    let action = 0;
+    let verb = 0;
+    let object = 0;
+    //the actual array that will be returned
+    let charQuirk = '';
+    //all of our super serial generation stuff
+    const seriousAction = ['has sworn', 'has given an oath', ]
+    const seriousVerb = ['to revenge', 'to defend']
+    const seriousObject = ['his brother']
+    //for the silly ones among us
+    const sillyAction = ['is obsessed with', 'is afraid of']
+    const sillyVerb = ['stealing', 'giving everyone', 'eating']
+    const sillyObject = ['left socks', 'salad forks', 'the good china', 'half-eaten meals', 'turkey']
+    //determines what serious/silly quirk the character has - needed to have seperate determinations due to the arrays not having the same length
+    if(charMood === 'serious'){
+        action = Math.floor(Math.random()*seriousAction.length)
+        verb = Math.floor(Math.random()*seriousVerb.length)
+        object = Math.floor(Math.random()*seriousObject.length)
+        
+        charQuirk = `${charName} the ${charRole} ${seriousAction[action]} ${seriousVerb[verb]} ${seriousObject[object]}`
+    } else if(charMood === 'silly'){
+        action = Math.floor(Math.random()*sillyAction.length)
+        verb = Math.floor(Math.random()*sillyVerb.length)
+        object = Math.floor(Math.random()*sillyObject.length)
+        
+        charQuirk = `${charName} ${sillyAction[action]} ${sillyVerb[verb]} ${sillyObject[object]}`
     }
+    return charQuirk;
+}
+
+//determines the race of the character. #todo - adjust stats based on the race of the character
+function generateRace(){
+    const raceArray = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling']
+    const r = Math.floor(Math.random()*raceArray.length);
+    return raceArray[r]
 }
